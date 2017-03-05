447847e625d1d95f69ab41389d24b133cfb5eaee
liumiaocn <liumiao.dl@gmail.com>
2017-03-05 16:46:20 +0800

    Create easypack_docker.sh

---
+++ docker/easypack_docker.sh
@@ -0,0 +1,147 @@
+#!/bin/sh
+###############################################################################
+#
+#VARS INIT
+#
+###############################################################################
+
+
+###############################################################################
+#
+#Confirm Env
+#
+###############################################################################
+date
+echo "## Install Preconfirm"
+echo "## Uname"
+uname -r
+echo "## OS bit"
+getconf LONG_BIT
+echo
+
+###############################################################################
+#
+#INSTALL yum-utils
+#
+###############################################################################
+date
+echo "## Install begins : yum-utils"
+yum install -y yum-utils >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Install failed..."
+  exit 1
+fi
+echo "## Install ends   : yum-utils"
+echo
+
+###############################################################################
+#
+#Setting yum-config-manager
+#
+###############################################################################
+echo "## Setting begins : yum-config-manager"
+yum-config-manager \
+   --add-repo \
+   https://download.docker.com/linux/centos/docker-ce.repo >/dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+  echo "Install failed..."
+  exit 1
+fi
+echo "## Setting ends   : yum-config-manager"
+
+###############################################################################
+#
+#Update Package Cache
+#
+###############################################################################
+echo "## Setting begins : Update package cache"
+yum makecache fast >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Install failed..."
+  exit 1
+fi
+echo "## Setting ends   : Update package cache"
+
+###############################################################################
+#
+#INSTALL Docker-engine
+#
+###############################################################################
+date
+echo "## Install begins : docker-ce"
+yum install -y docker-ce >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Install failed..."
+  exit 1
+fi
+echo "## Install ends   : docker-ce"
+date
+echo
+
+###############################################################################
+#
+#Stop Firewalld
+#
+###############################################################################
+echo "## Setting begins : stop firewall"
+systemctl stop firewalld
+if [ $? -ne 0 ]; then
+  echo "Install failed..."
+  exit 1
+fi
+systemctl disable firewalld
+if [ $? -ne 0 ]; then
+  echo "Install failed..."
+  exit 1
+fi
+echo "## Setting ends   : stop firewall"
+
+###############################################################################
+#
+#Clear Iptable rules
+#
+###############################################################################
+echo "## Setting begins : clear iptable rules"
+iptables -F
+if [ $? -ne 0 ]; then
+  echo "Install failed..."
+  exit 1
+fi
+echo "## Setting ends   : clear iptable rules"
+
+###############################################################################
+#
+#Enable docker
+#
+###############################################################################
+echo "## Setting begins : systemctl enable docker"
+systemctl enable docker
+if [ $? -ne 0 ]; then
+  echo "Install failed..."
+  exit 1
+fi
+echo "## Setting ends   : systemctl enable docker"
+
+
+###############################################################################
+#
+#start docker
+#
+###############################################################################
+echo "## Setting begins : systemctl start docker"
+systemctl start docker
+if [ $? -ne 0 ]; then
+  echo "Install failed..."
+  exit 1
+fi
+echo "## Setting ends   : systemctl start docker"
+
+
+###############################################################################
+#
+#confirm docker version
+#
+###############################################################################
+echo "## docker version"
+docker info
