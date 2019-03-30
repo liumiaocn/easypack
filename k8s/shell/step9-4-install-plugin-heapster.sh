4771c107ddefac2e5367e8111bb243e06e94b5f0
liumiao <liumiaocn@outlook.com>
2019-03-30 12:15:02 +0800

    set nodeport in install.cfg for heapster grafana component

--- k8s/shell/step9-4-install-plugin-heapster.sh
+++ k8s/shell/step9-4-install-plugin-heapster.sh
@@ -9,6 +9,13 @@ install_heapster(){
   check_image ${ENV_HEAPSTER_YAML_DIR}/${ENV_HEAPSTER_GRAFANA_YAML_FILE}
   check_image ${ENV_HEAPSTER_YAML_DIR}/${ENV_HEAPSTER_INFLUXDB_YAML_FILE}
 
+  echo "## the following keyword needs to be replaced"
+  grep -n ${ENV_HEAPSTER_GRAFANA_NODEPORT_KEYWORD} ${ENV_HEAPSTER_YAML_DIR}/${ENV_HEAPSTER_GRAFANA_YAML_FILE} 
+  echo
+
+  echo "## replace __HEAPSTER_GRAFANA_NODE_PORT__"
+  sed -i -e "s/__HEAPSTER_GRAFANA_NODE_PORT__/${ENV_HEAPSTER_GRAFANA_NODE_PORT}/" ${ENV_HEAPSTER_YAML_DIR}/${ENV_HEAPSTER_GRAFANA_YAML_FILE}
+
   reset_service ${ENV_HEAPSTER_YAML_DIR}
 
   echo "## begin check heapster, wait for ${ENV_DEFAULT_SLEEP_INTERVAL}s ..."
