a6a177af8ef4858bbacf141496e5fb70a153afc6
liumiaocn <liumiaocn@outlook.com>
2019-11-12 15:44:27 +0800

    Update init_csrf_disable.groovy

--- containers/alpine/jenkins/init_csrf_disable.groovy
+++ containers/alpine/jenkins/init_csrf_disable.groovy
@@ -2,5 +2,7 @@ import hudson.security.csrf.DefaultCrumbIssuer
 import jenkins.model.Jenkins
 
 def jenkins = Jenkins.instance;
+println "--> setting csrf disabled"
 jenkins.setCrumbIssuer(null); 
 jenkins.save();
+println "--> setting csrf disabled... done"
